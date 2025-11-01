f043403d858afd2429f2b14ee887f34891d8ceaf
AIheshen <heai58544@gmail.com>
2025-11-01 18:41:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     new Typed('#typing-effect', {
         strings: [ '我是艾合', '一位编程爱好者','期待与您合作','致力于将一切自动化', '专注于AI协作编程', '探索技术的无限可能'],
-        typeSpeed: 70,
+        typeSpeed: 80,
         backSpeed: 40,
         loop: true
     });
